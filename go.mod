2ca1afcc24c2046932b9215c9a0a85ed2e65fd4f
Reed Allman <reed@rdallman.com>
2019-04-09 14:05:59 -0700

    update with some stats

--- go.mod
+++ go.mod
@@ -4,4 +4,11 @@ require (
 	github.com/fnproject/fn_go v0.0.0-20181210161029-1c373f73ab3a
 	github.com/go-openapi/runtime v0.18.0
 	github.com/go-openapi/strfmt v0.18.0
+	github.com/gonum/blas v0.0.0-20181208220705-f22b278b28ac // indirect
+	github.com/gonum/floats v0.0.0-20181209220543-c233463c7e82 // indirect
+	github.com/gonum/internal v0.0.0-20181124074243-f884aa714029 // indirect
+	github.com/gonum/lapack v0.0.0-20181123203213-e4cdc5a0bff9 // indirect
+	github.com/gonum/matrix v0.0.0-20181209220409-c518dec07be9 // indirect
+	github.com/gonum/stat v0.0.0-20181125101827-41a0da705a5b
+	gonum.org/v1/gonum v0.0.0-20190409070159-6e46824336d2
 )
--- load.go
+++ load.go
@@ -1,19 +1,41 @@
 package main
 
 import (
-	fnclient "github.com/fnproject/fn_go/clientv2"
+	"bytes"
+	"context"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/fnproject/fn_go/clientv2"
+	"github.com/fnproject/fn_go/clientv2/apps"
+	"github.com/fnproject/fn_go/clientv2/fns"
 	models "github.com/fnproject/fn_go/modelsv2"
 	openapi "github.com/go-openapi/runtime/client"
 	"github.com/go-openapi/strfmt"
+	"gonum.org/v1/gonum/stat"
 )
 
 func main() {
+	fmt.Println(time.Now().Format(time.StampMilli))
 	n := flag.Int("n", 1, "number of invokes")
 	p := flag.Int("p", 1, "parallel threads")
 	appName := flag.String("app", "", "app name of function")
 	fnName := flag.String("fn", "", "name of function")
 	host := flag.String("host", "http://localhost:8080", "url of fn service")
 	// TODO(reed): take body from stdin
+	flag.Parse()
 
 	invokes := *n
 	threads := *p
@@ -24,12 +46,23 @@ func main() {
 	if invokes < 1 {
 		log.Fatal("n < 1")
 	}
+	if *appName == "" {
+		log.Fatal("app name must be non-empty string")
+	}
+	if *fnName == "" {
+		log.Fatal("fn name must be non-empty string")
+	}
 
 	// TODO(reed): this is awful, nobody should have to do this
-	transport := openapi.New(*host, path.Join("/", clientv2.DefaultBasePath), "http")
+	url, err := url.Parse(*host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	transport := openapi.New(url.Host, path.Join(url.Path, clientv2.DefaultBasePath), []string{url.Scheme})
 	client := clientv2.New(transport, strfmt.Default)
 
-	appsResp, err := client.Apps.ListApps(&apiapps.ListAppsParams{
+	appsResp, err := client.Apps.ListApps(&apps.ListAppsParams{
 		Context: context.Background(),
 		Name:    appName,
 	})
@@ -44,10 +77,10 @@ func main() {
 		log.Fatal("app not found")
 	}
 
-	resp, err := client.Fns.ListFns(&apifns.ListFnsParams{
+	resp, err := client.Fns.ListFns(&fns.ListFnsParams{
 		Context: context.Background(),
-		AppID:   app.ID,
-		Name:    *fn,
+		AppID:   &app.ID,
+		Name:    fnName,
 	})
 	if err != nil {
 		log.Fatal(err)
@@ -55,7 +88,7 @@ func main() {
 
 	var fn *models.Fn
 	for i := 0; i < len(resp.Payload.Items); i++ {
-		if resp.Payload.Items[i].Name == fnName {
+		if resp.Payload.Items[i].Name == *fnName {
 			fn = resp.Payload.Items[i]
 		}
 	}
@@ -63,31 +96,102 @@ func main() {
 		log.Fatal("fn not found")
 	}
 
+	httpClient := &http.Client{Transport: &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout:   10 * time.Second,
+			KeepAlive: 1 * time.Minute,
+		}).Dial,
+		TLSClientConfig: &tls.Config{
+			ClientSessionCache: tls.NewLRUClientSessionCache(8192),
+		},
+		TLSHandshakeTimeout:   10 * time.Second,
+		MaxIdleConnsPerHost:   512,
+		Proxy:                 http.ProxyFromEnvironment,
+		MaxIdleConns:          512,
+		IdleConnTimeout:       90 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}}
+
 	var wg sync.WaitGroup
+	var plot points
 	for i := 0; i < threads; i++ {
+		wg.Add(1)
 		go func() {
-			wg.Add(1)
 			defer wg.Done()
 			for j := 0; j < invokes/threads; j++ {
-				req, err := http.NewRequest("POST", host+"/invoke/"+fn.ID, nil)
+				req, err := http.NewRequest("POST", *host+"/invoke/"+fn.ID, nil)
 				if err != nil {
-					log.Error(err)
+					log.Println(err)
 				}
 				// TODO(reed):
 				req.Header.Set("Content-Type", "text/plain")
 
+				start := time.Now()
 				resp, err := httpClient.Do(req)
 				if err != nil {
-					log.Error(err)
+					log.Println(err)
 				}
+				end := time.Now()
+				plot = append(plot, point{start, end})
+
 				if resp.StatusCode != 200 {
-					log.Errorf("bad status code: %v", resp.StatusCode)
+					log.Printf("bad status code: %v", resp.StatusCode)
 					io.Copy(os.Stderr, resp.Body)
 				}
-				io.Copy(os.Stdout, resp.Body)
+				io.Copy(ioutil.Discard, resp.Body)
 			}
 		}()
 	}
 
 	wg.Wait()
+	fmt.Println(time.Now().Format(time.StampMilli))
+
+	sort.Sort(plot)
+	fmt.Println(plot)
+
+	// weight out the last 'threads' number - this shaves off cold start/freeze
+	var weights []float64
+	if len(plot) > threads {
+		plot = plot[:len(plot)-threads]
+	}
+
+	floats := plot.toFloats()
+	mean := time.Duration(stat.Mean(floats, weights))
+	_, stdf := stat.MeanStdDev(floats, weights)
+	std := time.Duration(stdf)
+	_, varif := stat.MeanVariance(floats, weights)
+	variance := time.Duration(varif)
+
+	median := plot[len(plot)/2].dur()
+
+	fmt.Println("max:", plot[len(plot)-1].dur(), "min:", plot[0].dur(), "mean:", mean, "median:", median, "std:", std, "variance:", variance)
+}
+
+type point struct {
+	start, end time.Time
+}
+
+func (p point) dur() time.Duration { return p.end.Sub(p.start) }
+
+type points []point
+
+func (p points) Len() int           { return len(p) }
+func (p points) Less(i, j int) bool { return p[i].end.Sub(p[i].start) < p[j].end.Sub(p[j].start) }
+func (p points) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+func (p points) String() string {
+	var b bytes.Buffer
+	for i, pp := range p {
+		fmt.Fprintln(&b, i, pp.end.Sub(pp.start), pp.start, pp.end)
+	}
+	return b.String()
+}
+
+// duration floats
+func (p points) toFloats() []float64 {
+	f := make([]float64, len(p))
+	for i, pp := range p {
+		f[i] = float64(pp.end.Sub(pp.start))
+	}
+	return f
 }
